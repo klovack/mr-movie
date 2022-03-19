1cd1ed5338c1fb00dd3bd483406dbb514c823ee6
Muhammad Rizki Fikriansyah <fikriansyahmr@gmail.com>
2022-03-19 14:22:13 +0100

    feat: add axios to be reusable when fetching through http

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,7 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello hello.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
---
+++ src/utils/requests.ts
@@ -0,0 +1,27 @@
+const API_KEY = process.env.TMDB_API_KEY_V4;
+
+export interface MovieRequestEndpoints {
+  fetchTrending: string;
+  fetchNetflixOriginals: string;
+  fetchTopRated: string;
+  fetchActionMovies: string;
+  fetchComedyMovies: string;
+  fetchHorrorMovies: string;
+  fetchRomanceMovies: string;
+  fetchDocumentaries: string;
+  fetchTVSeries: string;
+}
+
+const requests: MovieRequestEndpoints = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+  fetchTVSeries: `/discover/tv?api_key=${API_KEY}`,
+};
+
+export default requests;

9a556c0d56880ced37cf7bd9568b7090dbba490a
Muhammad Rizki Fikriansyah <fikriansyahmr@gmail.com>
2022-03-20 23:59:12 +0100

    feat: fully functional nav bar

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+import classNames from "classnames";
+import React, { useEffect, useState } from "react";
 import logo from "../../assets";
 import { Networks, NetworkUrls } from "../../types";
 import "./Nav.scss";
 
 function Nav() {
+  const [isScrolledDown, setIsScrolledDown] = useState(false);
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 100) {
+        setIsScrolledDown(true);
+      } else setIsScrolledDown(false);
+    };
+    window.addEventListener("scroll", scrollListener);
+
+    return () => {
+      window.removeEventListener("scroll", scrollListener);
+    };
+  }, []);
+
   return (
-    <div className="nav">
+    <div
+      className={classNames("nav", {
+        "nav__black-bg": isScrolledDown,
+      })}
+    >
       {Object.keys(Networks).map((networkId) => {
         return (
           <a

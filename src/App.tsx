4a397a1d4013a3dc47215d27525ea5804d458650
Muhammad Rizki Fikriansyah <fikriansyahmr@gmail.com>
2022-03-20 18:49:41 +0100

    feat: add banner from trending movie

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.scss";
+import Banner from "./components/Banner/Banner";
 import Row from "./components/Row/Row";
 import requests from "./utils/requests";
 
 function App() {
   return (
     <div className="App">
+      {/* NAV */}
+
+      <Banner />
+
       <Row title="Netflix Originals" url={requests.fetchNetflixOriginals} />
       <Row
         title="Trending Now"
@@ -16,7 +21,7 @@ function App() {
       <Row title="Action" url={requests.fetchActionMovies} />
       <Row title="Comedy" url={requests.fetchComedyMovies} />
       <Row title="Horror" url={requests.fetchHorrorMovies} />
-      <Row title="Romance" url={requests.fetchHorrorMovies} />
+      <Row title="Romance" url={requests.fetchRomanceMovies} />
       <Row
         title="Documentaries You Might Like"
         url={requests.fetchDocumentaries}

3d9bd3ed9ad60a7bf0450d9ae6ec55b8ed434918
Muhammad Rizki Fikriansyah <fikriansyahmr@gmail.com>
2022-03-20 23:42:58 +0100

    feat: add basic navbar

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,10 +1,26 @@
 import React from "react";
-import logo from "../../assets/logo.png";
+import logo from "../../assets";
+import { Networks, NetworkUrls } from "../../types";
+import "./Nav.scss";
 
 function Nav() {
   return (
     <div className="nav">
-      <img src={logo} alt="Netflix Logo" className="nav__logo" />
+      {Object.keys(Networks).map((networkId) => {
+        return (
+          <a
+            key={networkId}
+            className="nav__link"
+            href={NetworkUrls[networkId]}
+          >
+            <img
+              src={logo[networkId]}
+              alt="Netflix Logo"
+              className="nav__logo"
+            />
+          </a>
+        );
+      })}
     </div>
   );
 }
